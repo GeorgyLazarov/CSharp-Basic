efa44fb089004fbc1f00b3969eb05ae095f3281e
Georgi Lazarov <134303144+GeorgyLazarov@users.noreply.github.com>
2023-06-10 11:50:42 +0300

    For Loop

---
+++ For Loop - Lab/06. Vowels Sum/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _06._Vowels_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine().ToLower();
+
+            int vowelsSum = 0;
+            for (int i = 0;i< text.Length;i++) 
+            {
+                char currentChar= text[i];
+                if (currentChar == 'a')
+                {
+                    vowelsSum += 1;
+                }
+                else if (currentChar == 'e')
+                {
+                    vowelsSum += 2;
+                }
+                else if (currentChar == 'i')
+                {
+                    vowelsSum += 3;
+                }
+                else if (currentChar == 'o')
+                {
+                    vowelsSum += 4;
+                }
+                else if (currentChar == 'u')
+                {
+                    vowelsSum += 5;
+                }
+              
+            }
+            Console.WriteLine(vowelsSum);
+
+            
+        }
+    }
+}
---
+++ For Loop - Lab/10. Odd Even Sum/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace _10._Odd_Even_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double n = int.Parse(Console.ReadLine());
+
+            int leftSum = 0;
+            int rightSum = 0;
+            for (int i =0; i < Math.Floor(n/2); i++)
+            {
+
+                int even = int.Parse(Console.ReadLine());
+                if (i%2==0)
+                {
+                 leftSum = leftSum + even;
+                }
+                else 
+                    rightSum= rightSum + even;
+            }
+
+            for (int i = 1; i <= (n/2); i ++)
+            {
+                int even = int.Parse(Console.ReadLine());
+                if (i % 2 != 0)
+                {
+                    rightSum = rightSum + even;
+                }
+                else 
+                {
+                    leftSum = leftSum + even;
+                }
+
+            }
+           
+            int difference = Math.Abs(leftSum - rightSum);
+            if (difference == 0)
+            {
+                Console.WriteLine($"Yes, sum = {rightSum}");
+            }
+            else
+            {
+                Console.WriteLine($"No, diff = {difference}");
+            }            
+        }
+    }
+}
